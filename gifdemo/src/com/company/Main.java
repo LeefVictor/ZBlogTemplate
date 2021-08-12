0e52d22bad1ac2087141ea0456ec5cad63f0c6e1
LeefVictor <254690663@qq.com>
2021-08-12 11:01:41 +0800

    update gifdemo

--- gifdemo/src/com/company/Main.java
+++ gifdemo/src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
+import com.company.image.GifBuilder;
+import com.company.image.SplitSprites;
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+        SplitSprites ss = new SplitSprites("D:\\temp\\sprites.png", "D:\\temp\\gif\\split").scanByCol().split().save();
+        new GifBuilder(ss.getImagesAfterSplit(), true, 10, "D:\\temp\\gif\\test.gif").build();
     }
 }
--- gifdemo/src/com/company/image/SplitSprites.java
+++ gifdemo/src/com/company/image/SplitSprites.java
@@ -124,11 +124,6 @@ public class SplitSprites {
         return newImage;
     }
 
-    public static void main(String[] args) throws IOException {
-        SplitSprites ss = new SplitSprites("D:\\temp\\sprites.png", "D:\\temp\\gif\\split").scanByCol().split().save();
-        new GifBuilder(ss.getImagesAfterSplit(), true, 10, "D:\\temp\\gif\\test.gif").build();
-    }
-
 
     static class Img {
 
